05790ce2112cb3b41e4b5af1769d59b1b58a9d0b
andycabrera <andycaabrera@users.noreply.github.com>
2020-04-01 18:39:47 -0300

    Volque los datos del usuario en perfil

--- app/src/main/java/com/devthion/myapplication/Perfil.java
+++ app/src/main/java/com/devthion/myapplication/Perfil.java
@@ -1,15 +1,56 @@
 package com.devthion.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Perfil extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
+    String userID,userNombre;
+    TextView txtId,etPerfilNombre;
+    //DECLARAMOS LA BASE DE DATOS DE LOS USUARIOS
+    FirebaseFirestore databaseUsuarios;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_perfil);
+        txtId =(TextView) findViewById(R.id.txtPerfilId);
+        etPerfilNombre =(TextView) findViewById(R.id.etPerfilNombre);
+        databaseUsuarios = FirebaseFirestore.getInstance();
+
+        fAuth = FirebaseAuth.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = databaseUsuarios.collection("Usuarios").document(userID);
+
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()){
+                    userNombre = (String) documentSnapshot.getData().get("Nombre");
+                    etPerfilNombre.setText("NOMBRE DEL USUARIO " + userNombre);
+                }
+            }
+        });
+
+
+
+
+        txtId.setText("ID "+userID);
 
 
     }
