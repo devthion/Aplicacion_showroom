603fa48b52631ff74d16f8beb298d1cd03fc3b1f
ElDieguii <49466415+ElDieguii@users.noreply.github.com>
2020-04-03 00:37:03 -0300

    Agrego fragmento cupon a el menu de admin

--- app/src/main/java/com/devthion/myapplication/Cupones.java
+++ app/src/main/java/com/devthion/myapplication/Cupones.java
@@ -1,4 +1,61 @@
 package com.devthion.myapplication;
 
-public class Cupones {
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class Cupones extends AppCompatActivity {
+    DatabaseReference databaseCupones;
+    FirebaseAuth fAuth;
+    String prueba = "";
+    TextView pantalla;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_cupones);
+
+        pantalla = findViewById(R.id.pantalla);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        //Referencio la tabla "usuarios"
+        databaseCupones= FirebaseDatabase.getInstance().getReference("usuarios");
+
+
+        //el add value event listener, se va a ejecutar cada vez que haya un ingreso de valor en la tabla
+        //o sea si se actualiza un valor, se va a ejecutar el codigo
+        databaseCupones.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //for para recorrer dentro de usuarios, todos los usuario y asi acceder a sus cupones
+                for (DataSnapshot unUsuario : dataSnapshot.getChildren()) {
+                        //for para recorrer los cupones de unUsuario
+                        for(DataSnapshot unCupon: unUsuario.child("cupones").getChildren() ){
+                            prueba = prueba +unCupon.getValue().toString()+"\n";
+                            pantalla.setText(prueba);
+                        }
+
+
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/devthion/myapplication/FragmentMenuPrincipal.java
+++ app/src/main/java/com/devthion/myapplication/FragmentMenuPrincipal.java
@@ -17,9 +17,6 @@ public class FragmentMenuPrincipal extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        fAuth = FirebaseAuth.getInstance();
-        pantalla = getView().findViewById(R.id.pantalla);
-        pantalla.setText(""+fAuth.toString());
         return inflater.inflate(R.layout.fragment_fragment_menu_principal, container, false);
     }
 
