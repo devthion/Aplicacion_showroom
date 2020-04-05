8dc33fca5c6710dd31c0610f88ec161ca59cf240
ElDieguii <49466415+ElDieguii@users.noreply.github.com>
2020-04-05 18:06:59 -0300

    Diseño del menu principal

--- app/src/main/java/com/devthion/myapplication/FragmentMenuPrincipal.java
+++ app/src/main/java/com/devthion/myapplication/FragmentMenuPrincipal.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +20,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class FragmentMenuPrincipal extends Fragment  {
 
-    TextView txtPrueba;
+    ImageView fondo_menu_principal;
+    LinearLayout textVMenu;
     FirebaseAuth fAuth;
     Button btnCerrarSesion;
 
@@ -29,13 +32,19 @@ public class FragmentMenuPrincipal extends Fragment  {
         View view = inflater.inflate(R.layout.fragment_fragment_menu_principal, container, false);
         btnCerrarSesion = (Button) view.findViewById(R.id.btnCerrarSesionn);
         fAuth = FirebaseAuth.getInstance();
-        txtPrueba = (TextView) view.findViewById(R.id.txtPrueba);
+        fondo_menu_principal = view.findViewById(R.id.fondo_menu_principal);
+        textVMenu =view.findViewById(R.id.linearMenu);
+
+
+
+        fondo_menu_principal.animate().translationY(-2500).setDuration(1000).setStartDelay(300);
+        textVMenu.animate().translationY(140).alpha(0).setDuration(1000).setStartDelay(300);
 
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //fAuth.signOut();
-                txtPrueba.setText("asdasd");
+
                 Toast.makeText(getActivity(), "CERRAR SESION", Toast.LENGTH_SHORT).show();
                 //Intent in = new Intent(getActivity(), Perfil.class);
                 //startActivity(in);
