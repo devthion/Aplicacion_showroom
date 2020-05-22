cfdd517db4c38fbdfb98d53580da92fee166a2fa
ElDieguii <49466415+ElDieguii@users.noreply.github.com>
2020-05-22 18:00:58 -0300

    Diseño de detalleLocal + boton dar reseña

--- app/src/main/java/com/devthion/myapplication/DetalleLocal.java
+++ app/src/main/java/com/devthion/myapplication/DetalleLocal.java
@@ -1,15 +1,20 @@
 package com.devthion.myapplication;
 
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.devthion.myapplication.Interfaces.InterfaceBusquedaUnLocal;
@@ -19,9 +24,10 @@ import com.devthion.myapplication.modelos.Local;
 import java.util.ArrayList;
 
 
-public class DetalleLocal extends AppCompatActivity {
+public class DetalleLocal extends AppCompatActivity implements View.OnClickListener {
 
     TextView textV_nombreLocal, textV_descripcionLocal;
+    Button btn_dar_calificacion;
     BusquedaDeLocalesFirebase busquedaDeLocalesFirebase = new BusquedaDeLocalesFirebase();
 
 
@@ -32,6 +38,7 @@ public class DetalleLocal extends AppCompatActivity {
 
 
         textV_descripcionLocal=findViewById(R.id.textV_descripcionLocal);
+        btn_dar_calificacion=findViewById(R.id.btn_dar_calificacion);
 
         Intent intent = getIntent();
         String idLocal = intent.getStringExtra("idLocal");
@@ -46,9 +53,48 @@ public class DetalleLocal extends AppCompatActivity {
         });
 
 
-
+        btn_dar_calificacion.setOnClickListener(this);
 
 
 
     }
+
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_dar_calificacion:
+                alertDialog();
+            break;
+        }
+    }
+
+    //CREAR UNA CLASE DE ALERTDIALOG CON METODO "MOSTRAR"
+    public void alertDialog(){
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+
+                .setIcon(android.R.drawable.ic_dialog_alert)
+
+                .setTitle("Are you sure to Exit")
+
+                .setMessage("Exiting will call finish() method")
+
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //set what should happen when negative button is clicked
+                        Toast.makeText(getApplicationContext(),"Nothing Happened",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .show();
+    }
+
+
+
 }
